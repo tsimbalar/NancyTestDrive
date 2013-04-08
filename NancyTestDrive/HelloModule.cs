e4276c6396662264b49f28a3e508505cc17fd8a0
Thibaud Desodt <tibo.desodt@gmail.com>
2013-04-08 18:44:57 +0200

    playing with routing

--- NancyTestDrive/HelloModule.cs
+++ NancyTestDrive/HelloModule.cs
@@ -11,6 +11,24 @@ namespace NancyTestDrive
         public HelloModule()
         {
             Get["/"] = parameters => "Hello World";
+
+            // would capture routes like /hello/nancy sent as a GET request
+            Get["/hello/{name}"] = parameters =>
+            {
+                return "Hello " + parameters.name;
+            };
+
+            // would capture routes like /products/1034 sent as a DELETE request
+            Delete[@"/products/(?<id>[\d]{1,7})"] = parameters =>
+            {
+                return 200;
+            };
+
+            // would capture routes like /users/192/add/moderator sent as a POST request
+            Post["/users/{id}/add/{category}"] = parameters =>
+            {
+                return HttpStatusCode.OK;
+            };
         }
     }
 }
---
+++ NancyTestDrive/ResourceModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyTestDrive
+{
+    public class ResourceModule : NancyModule
+    {
+        public ResourceModule()
+            : base("/products")
+        {
+            // would capture routes to /products/list sent as a GET request
+            Get["/list"] = parameters =>
+            {
+                return "The list of products";
+            };
+        }
+    }
+}
