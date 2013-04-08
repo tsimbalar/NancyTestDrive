d234b2b33ab55e631ad3fe412ba7f1fbdf027e84
Thibaud Desodt <tibo.desodt@gmail.com>
2013-04-08 18:29:36 +0200

    working nancy application!!

---
+++ NancyTestDrive/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyTestDrive
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
