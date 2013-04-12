ce81f34ae32230e4e10bd3527f654123e1e8e1e8
Thibaud Desodt <tibo.desodt@gmail.com>
2013-04-12 17:09:16 +0200

    example of static files

---
+++ NancyTestDrive/CustomBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Conventions;
+
+namespace NancyTestDrive
+{
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+
+            // make /js server files from /_scripts
+            conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("js", "_scripts", "js" /* only js extension is allowed */) 
+                );
+
+
+        }
+    }
+}
