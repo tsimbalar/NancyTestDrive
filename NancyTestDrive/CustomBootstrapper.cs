cc0767f24bc53a5758ee8b0fce9595cda1ed4467
Thibaud Desodt <tibo.desodt@gmail.com>
2013-04-15 10:28:40 +0200

    playing with Diagnostics

--- NancyTestDrive/CustomBootstrapper.cs
+++ NancyTestDrive/CustomBootstrapper.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Conventions;
+using Nancy.Diagnostics;
+using Nancy.TinyIoc;
 
 namespace NancyTestDrive
 {
     public class CustomBootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            StaticConfiguration.EnableRequestTracing = true;
+        }
+
+        protected override Nancy.Diagnostics.DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get
+            {
+                return new DiagnosticsConfiguration
+                    {
+                        Password = "password"
+                    };
+            }
+        }
+
+
         protected override void ConfigureConventions(NancyConventions conventions)
         {
             base.ConfigureConventions(conventions);
@@ -16,10 +37,10 @@ namespace NancyTestDrive
 
             // make /js server files from /_scripts
             conventions.StaticContentsConventions.Add(
-                StaticContentConventionBuilder.AddDirectory("js", "_scripts", "js" /* only js extension is allowed */) 
+                StaticContentConventionBuilder.AddDirectory("js", "_scripts", ".js" /* only js extension is allowed */) 
                 );
-
-
         }
+
+
     }
 }
---
+++ NancyTestDrive/ThibaudDiagnosticProvider.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.Diagnostics;
+
+namespace NancyTestDrive
+{
+    public class ThibaudDiagnosticProvider : IDiagnosticsProvider
+    {
+        public string Name { get { return "Thibaud's diags"; } }
+        public string Description { get { return @"It does stuff ..."; } }
+        public object DiagnosticObject { get { return new ThibaudDiagnostic(); } }
+    }
+
+    public class ThibaudDiagnostic
+    {
+        public string AddNumbers(int number1, int number2)
+        {
+            return String.Format("{0} + {1} = {2}", number1, number2, number1 + number2);
+        }
+
+        // default convention : look for a property with NameOfMethod + Description ....
+        // or use a Nancy.Diagnostics.DescriptionAttribute on the method ...
+        public string AddNumbersDescription{get { return "Adds 2 numbers and displays the addition ..."; }}
+
+        // default convention : look for a property with NameOfMethod + Template ....
+        // or use a Nancy.Diagnostics.TemplateAttribute on the method ...
+        public string AddNumbersTemplate { get { return "<h1>Addition result</h1><p style=\"text-decoration:underline; color:red;\">{{model.Result}}</p>"; } }
+
+        [Description("Shows a list of \"count\" Foos ...")]
+        public List<Foo> ShowFoos(int count = 5)
+        {
+            var result = new List<Foo>();
+            for (int i = 0; i < count; i++)
+            {
+                var f = new Foo(i);
+                result.Add(f);
+            }
+
+            return result;
+        }
+    }
+
+    public class Foo
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly List<Bar> _bars;
+
+        public Foo(int id)
+        {
+            _id = id;
+            _name = "I am foo " + id;
+            _bars = new List<Bar>();
+            _bars.AddRange(Enumerable.Range(0, id).Select(index => new Bar(index)));
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public List<Bar> Bars
+        {
+            get { return _bars; }
+        }
+    }
+
+    public class Bar
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Bar(int index)
+        {
+            _age = index;
+            _name = "I am Bar #" + index;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Age { get { return _age; } }
+    }
+}
