066045f7f0ca3c7e1a0eefd15705313bc750f705
Thibaud Desodt <tibo.desodt@gmail.com>
2013-04-15 14:16:38 +0200

    Example with WCF Hosting

---
+++ NancyWcfApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Wcf;
+using NancyTestDrive;
+
+namespace NancyWcfApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string bindToAddress = "http://localhost:1234/nancy/";
+
+            var t = typeof (HelloModule); // force reference to other project where Modules are defined
+
+            var host = new WebServiceHost(new NancyWcfGenericService(), new Uri(bindToAddress));
+            host.AddServiceEndpoint(typeof (NancyWcfGenericService), new WebHttpBinding(), "");
+            
+            host.Open();
+
+            Console.WriteLine(string.Format("Listening at adress {0} ... press a key to stop", bindToAddress));
+            Console.Read();
+
+            host.Close();
+
+
+        }
+
+        private static Type ForceReferenceToAssemblyWithModules = typeof (HelloModule);
+    }
+}
